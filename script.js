7c05bf4c3d1744e4952290f0c7504c81df493240
oZanella <henriquezanella19@gmail.com>
2024-09-14 12:46:59 -0300

    Finalizado o button de troca de mode, também iniciado o JS com a funciton

---
+++ script.js
@@ -0,0 +1,11 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // if(html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // }else{
+  //   html.classList.add('light')
+  // }
+
+}
