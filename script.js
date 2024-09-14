2b9b649ebaa9d87c8e53c5ad9db5eba860bcbc8e
oZanella <henriquezanella19@gmail.com>
2024-09-14 13:57:21 -0300

    Finalizado o JS com alteração de img e alt para cada modo

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 function toogleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
 
-  // if(html.classList.contains('light')) {
-  //   html.classList.remove('light')
-  // }else{
-  //   html.classList.add('light')
-  // }
+  //pegar a tag img
+  const img = document.querySelector("#profile img")
 
+  //substituir a img
+  if (html.classList.contains("light")) {
+    //se tiver ligth mode, adicionar img ligth
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Sem foto no modo light")
+  } else {
+    //se tiver sem ligth mode, adicionar img normal
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Sem foto no modo Dark")
+  }
 }
