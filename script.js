2fed79a1808edf6a95ff4b8e2c784e38243f0128
oZanella <henriquezanella19@gmail.com>
2024-09-15 14:39:42 -0300

    Alterado o avatar para modo dark e light

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ function toogleMode() {
   //substituir a img
   if (html.classList.contains("light")) {
     //se tiver ligth mode, adicionar img ligth
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-light.png")
     img.setAttribute("alt", "Sem foto no modo light")
   } else {
     //se tiver sem ligth mode, adicionar img normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/avatar-dark.png")
     img.setAttribute("alt", "Sem foto no modo Dark")
   }
 }
